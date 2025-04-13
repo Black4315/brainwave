b25cb4b18b0e3d7f71a7014fd616768b963144de
Black4315 <yurytube4315@gmail.com>
2025-04-13 19:33:16 +0200

    seo & scrollprallex

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -57,11 +57,11 @@ const Hero = () => {
                                     height={490}
                                     alt="AI"
                                 />
-                                <Generating className={'absolute bottom-5 right-4 md:left-1/2 md:bottom-8 md:right-auto md:w-[31rem] md:-translate-x-1/2'}/>
+                                <Generating className={'absolute bottom-5 left-1/2 md:bottom-8 md:right-auto md:w-[31rem] w-max -translate-x-1/2'}/>
 
                                 <ScrollParallax isAbsolutelyPositioned>
-                                    <ul className="hidden xl:flex absolute -left-[5.5rem] 
-                                bottom-[7.5rem] px-1 py-1  bg-n-9/40 rounded-2xl border border-n-1/10 backdrop-blur">
+                                    <ul className="flex absolute xl:-left-[5.5rem] -left-[2rem]
+                                    bottom-[5.5rem] sm:bottom-[7.5rem] px-1 py-1  bg-n-9/40 rounded-2xl border border-n-1/10 backdrop-blur">
                                         {heroIcons.map((icon, index) => (
                                             <li className="p-5" key={index}>
                                                 <img src={icon} width={24} height={25} alt={icon} />
@@ -71,7 +71,7 @@ const Hero = () => {
                                 </ScrollParallax>
                                 <ScrollParallax isAbsolutelyPositioned>
                                     <Notification
-                                        className="hidden absolute -right-[5.5rem] bottom-[11rem] w-[18rem] xl:flex"
+                                        className="absolute xl:-right-[5.5rem] -right-[2rem] bottom-[13rem] sm:bottom-[11rem] w-[18rem] flex"
                                         title="Code generation"
                                     />
                                 </ScrollParallax>
